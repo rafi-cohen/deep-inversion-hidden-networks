c3f8abef687ef7e146864e99b0d9b52100c2e15e
Adam Botach <adambo@outlook.com>
2019-11-09 21:22:31 +0200

    part2 - q1 answer

--- hw1/hw1/answers.py
+++ hw1/hw1/answers.py
@@ -12,13 +12,14 @@ part2_q1 = r"""
 **Your answer:**
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+It appears that the best accuracy scores were achieved using relatively low values of k (1-5), while the best score was
+achieved using `k = 3`.
 
+On the one hand, by basing its prediction on a very small subset of the sample's closest neighbors the model might tend
+to overfit to the training data, which would lead to worse generalization for unseen data. On the other hand, basing
+the model's prediction on a very large subset of the neighbors might introduce unwanted noise into the classification 
+process and ultimately lower the model's accuracy. This would happen because in this case unrelated neighbors that are
+farther away from the sample might have too much impact on the model's prediction.
 """
 
 part2_q2 = r"""
