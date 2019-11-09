311b65561549165f8d0a5b68757eeee315380211
Adam Botach <adambo@outlook.com>
2019-11-09 14:51:02 +0200

    transformations

--- hw1/hw1/transforms.py
+++ hw1/hw1/transforms.py
@@ -28,7 +28,7 @@ class InvertColors(object):
         """
         # TODO: Invert the colors of the input image.
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        return 1. - x
         # ========================
 
 
@@ -40,7 +40,7 @@ class FlipUpDown(object):
         """
         # TODO: Flip the input image so that up is down.
         # ====== YOUR CODE: ======
-        raise NotImplementedError()
+        return torch.flip(x, [1])
         # ========================
 
 
