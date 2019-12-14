Fix error in dimension calculation (ConvClassifier->_make_classifier)

hw2/hw2/cnn.py
@@ -88,8 +88,9 @@ class ConvClassifier(nn.Module):
         #  the first linear layer.
         #  The last Linear layer should have an output dim of out_classes.
         # ====== YOUR CODE: ======
+        N = len(self.channels)
         P = self.pool_every
-        prev_out_dim = ((in_h * in_w) // 2**(2*P)) * self.channels[-1]
+        prev_out_dim = ((in_h * in_w) // 2**(2*(N // P))) * self.channels[-1]
         for out_dim in self.hidden_dims:
             layers.append(nn.Linear(in_features=prev_out_dim, out_features=out_dim))
             layers.append(nn.ReLU())