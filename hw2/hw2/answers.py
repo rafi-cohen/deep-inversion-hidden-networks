6e7e05b97fa5c58b514aee4cf4f012aff3ba6b3b
Adam Botach <adambo@outlook.com>
2019-12-17 22:43:55 +0200

    Part 2 Q1 answer

--- hw2/hw2/answers.py
+++ hw2/hw2/answers.py
@@ -49,14 +49,21 @@ def part2_dropout_hp():
 
 part2_q1 = r"""
 **Your answer:**
+1. Our expectation was that the models using dropout would be less overfitted than the one without, which 
+would translate to better generalization (better accuracy) on the test set. 
 
+    The graphs did in fact match our expectation: While the model
+without the dropout reached almost 100% accuracy on the training set, it performed a
+lot worse on the test set than the models with the dropout, even though these models did not perform as well
+on the training set. This proves that the model without the dropout was extremely overfitted, which indicates
+that using dropout does prevent overfitting and thus leads to better generalization.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. The graphs indicate that during the training phase the low-dropout setting performed significantly better
+than the high-dropout setting. This might lead to think that the low-dropout setting is more overfitted than 
+the high-dropout one, but the graph of the test accuracy indicates otherwise: It seems that the low-dropout model
+was able to converge faster than the high-dropout model, which shows that the lower value is a good compromise
+between not using dropout at all, and using dropout at a probability that is too high. We expect however that the best
+dropout value would be somewhere in between the 2 values we used.
 
 """
 
