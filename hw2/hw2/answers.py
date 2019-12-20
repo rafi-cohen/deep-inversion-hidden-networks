1451aa58022b746d6835884ab731bec8935ea9ce
Adam Botach <botach@campus.technion.ac.il>
2019-12-20 20:21:02 +0200

    Part 3 Q5 answer and some changes to other questions

--- hw2/hw2/answers.py
+++ hw2/hw2/answers.py
@@ -53,17 +53,17 @@ part2_q1 = r"""
 would translate to better generalization (better accuracy) on the test set. 
 
     The graphs did in fact match our expectation: While the model
-without the dropout reached almost 100% accuracy on the training set, it performed a
+without the dropout reached almost 90% accuracy on the training set, it performed a
 lot worse on the test set than the models with the dropout, even though these models did not perform as well
 on the training set. This proves that the model without the dropout was extremely overfitted, which indicates
 that using dropout does prevent overfitting and thus leads to better generalization.
 
 2. The graphs indicate that during the training phase the low-dropout setting performed significantly better
-than the high-dropout setting. This might lead to think that the low-dropout setting is more overfitted than 
-the high-dropout one, but the graph of the test accuracy indicates otherwise: It seems that the low-dropout model
-was able to converge faster than the high-dropout model, which shows that the lower value is a good compromise
-between not using dropout at all, and using dropout at a probability that is too high. We expect however that the best
-dropout value would be somewhere in between the 2 values we used.
+than the high-dropout setting (~65% vs. ~30%). This, along with the test accuracy results (~28% vs. ~25%) perhaps
+indicate that the low-dropout model is more overfitted than the high-dropout one, but since the lower model achieved
+better test accuracy while also converging faster, we think it makes a pretty good compromise between not using dropout
+at all, and using dropout at a probability that is too high. We expect however that the ideal dropout value in this case
+would be somewhere in between the 2 values we used.
 
 """
 
@@ -71,9 +71,9 @@ part2_q2 = r"""
 **Your answer:**
 Yes, it is possible. Example:
 Assuming we have 2 samples `x1`,`x2` with ground-truth labels: `Y1`,`Y2` respectively.
-It is possible that in epoch `t` the probability vectors (in the form $[Y1_{score}, Y2_{score}]$) we get for each sample
-are: $[0.4, 0.6]$ for `x1` and $[0, 1]$ for `x2`. Therefore both samples would be classified as belonging to `Y2` which
-means: accuracy = 50%, while the CE loss is: $\text{loss}=-\log(0.4)-\log(1)=0.916$.
+It is possible that in epoch `t` the probability vectors (in the form $[Y1_{score}, Y2_{score}]$) we would get for each
+sample are: $[0.4, 0.6]$ for `x1` and $[0, 1]$ for `x2`. Therefore both samples would be classified as belonging to `Y2`
+which means: accuracy = 50%, while the CE loss is: $\text{loss}=-\log(0.4)-\log(1)=0.916$.
 
 Later, it is also possible that in epoch `t+1` the probability vectors we get for each sample
 are: $[0.6, 0.4]$ for `x1` and $[0.4, 0.6]$ for `x2`. Therefore both samples would be classified correctly which
@@ -92,56 +92,51 @@ part3_q1 = r"""
 
 1. It appears that the network achieved better accuracy using smaller values of `L` (depth).
 The value of `L` which was used to achieve the best results was 2. Also, it appears that in some cases
-the network was unable to learn at all using larger values of `L`. We suspected that the reason for that
-was the exploding/vanishing gradient phenomenon which we discussed in class. After looking into the matter,
-we discovered that we are dealing here with vanishing gradient.
+the network was unable to learn at all using larger values of `L`. We suspect that the reason for that
+is the exploding/vanishing gradient phenomenon which we discussed in class. After looking into the matter,
+we discovered that we were in fact dealing here with vanishing gradients.
 
-2. The values of `L` with witch the network was unable to learn are `L`=16 for both values of `K`,
-and `L`=8 for `K`=32. As stated above, the reason for that is the vanishing gradient phenomenon, which
-gets worse in deeper networks. This happens because in these networks the gradients are multiplied more times
-than in shallow networks (because of the extra layers). Thus, if the magnitudes of the values in the
+2. The values of `L` with which the network was unable to learn are `L=16` for both values of `K`,
+and `L=8` for `K=32`. As stated above, the reason for that is the vanishing gradient phenomenon, which
+gets worse in deeper networks. This happens because in deeper networks the gradients are multiplied more times
+than in shallow networks (because of the extra layers). Thus, if the magnitudes of the values in these
 multiplications are too small (between 0 and 1), in the deeper networks the product of many small values
 might get too close to 0 which would prevent these networks from learning properly. 
 
-    One way to resolve this issue is using batch-normalization, which helps stabilizing the gradients
+    One way to try to resolve this issue is using batch-normalization, which helps stabilizing the gradients
 as we discussed in class.
 
     Another way is to use residual networks. These networks use skip connections, which might help
-mitigate the problems because the gradients are able to flow backwards more freely through the skip
-connections and are thus less prone to vanishing.
+mitigate the problem by allowing the gradients to flow backwards more freely through the skip
+connections and thus making them less prone to vanishing.
 """
 
 part3_q2 = r"""
 **Your answer:**
 
 By looking at the graphs, it appears that large values of `K` (large number of filters) don't work well
-in shallow networks while smaller values work fine. For example, in the case of `L`=2 and `K`=256, the network
+in shallow networks while smaller values work fine. For example, in the case of `L=2` and `K=256`, the network
 achieved significantly worse results, compared to configurations with other values of `K` with the same `L`.
 On the other hand, it appears that in deeper networks, smaller values of `K` don't work so well while larger
-values work fine. For example, for `L`=4 and `L`=8 the networks using the smallest possible value of
- K` (32) achieved the worst results.
+values work fine. For example, for `L=4` and `L=8` the networks using the smallest possible value of
+`K` (32) achieved the worst results.
  
 These two conclusions might hint of a positive linear connection between the number of filters used
 in the networks and how deep it should be in order to get good results.
  
-It appears, in both experiments, 1.1 and 1.2, that the results of the deeper networks (`L`=8 and above)
-were not as good as the results of the shallower ones: in some cases, the deeper networks were not able
-to learn at all, and in other cases they achieved lower test accuracy scores. For example, in the case
-of `L`=8, all networks did not achieve more than 65% regardless of the value of `K`, while networks
-using lower values of `L` did get up to 70% for some values of `K`. A possible explanation for this
-phenomenon is again the possibility that the gradients in the deeper networks are less stable compared
-to the shallower networks.
+It appears, in both experiments 1.1 and 1.2, that in some cases deeper networks were not able to learn. Again, this is a
+direct result of the vanishing gradients phenomenon which we explained in our answer to Q1.
 
 """
 
 part3_q3 = r"""
 **Your answer:**
 
-Not surprisingly, the best results in experiment 1.3 were achieved by the shallowest network (`L`=1) with
-the three convolutional layers. Not only did this network achieve the best accuracy score, it also had the
-fastest convergence rate (achieved over 70% accuracy in about 5 epochs, while for `L`=3 it took over 20
-epoch to reach the same accuracy). So overall it seems that the deeper the network, the slower its convergence
-rate was. Also, once again, it seems that once the network gets too deep (`L`=4, with 12 layers) it is unable 
+Not surprisingly, the best results in experiment 1.3 were achieved by the shallowest networks (`L=1,2`) with
+the three/six convolutional layers. Not only did these networks achieve the best accuracy scores, they also had the
+fastest convergence rate (`L=1` achieved over 70% accuracy in about 5 epochs, `L=2` in about 9 epochs, while it took
+`L=3` over 20 epochs to reach the same accuracy). So overall it seems that the deeper the network, the slower its
+convergence rate is. Also, once again, it seems that once the network gets too deep (`L=4`, with 12 layers) it is unable 
 to learn due to the vanishing gradient phenomenon.
 
 """
@@ -150,32 +145,49 @@ part3_q4 = r"""
 **Your answer:**
 
 Once again, it seems that the shallowest networks achieved the best results, but this time by a significantly
-wider margin. For example, for `K`=32, the network using `L`=8 achieved about 12% better test accuracy than the
-networks with the larget `L` values. While in the case of `K`=[64, 128, 256], the network with `L`=8 acheived
+wider margin. For example, for `K=32`, the network using `L=8` achieved about 12% better test accuracy than the
+networks with the largest `L` values. While in the case of `K=[64, 128, 256]`, the network with `L=2` achieved
 the best results and also converged the fastest. It should also be noted that for the first time, results close
 to 80% test accuracy were seen, which illustrates the advantage of ResNets over regular CNNs.
 
-Also, the results of this experiment justify that our previous suggestion for using ResNets to as a way to deal
-with the vanishing gradient phenomenon. For example, while in experiment 1.1, the (`L`=8, `K`=32) network was
-completely unable to learn because of above phenomenon, the same architecture using ResNet performed very well,
+Also, the results of this experiment justify our previous suggestion for using ResNets as a way to deal
+with the vanishing gradient phenomenon. For example, while in experiment 1.1, the (`L=8`, `K=32`) network was
+completely unable to learn because of the above phenomenon, the same architecture using ResNet performed very well,
 and was able to achieve 70% test accuracy.
 
-Furthermore, compared to experiment 1.3, where the 12-layer (`L`=4, `K`=[64,128,256]) network was unable to learn,
-the same architecture using ResNet again performed very well, and even the 24-layer (`L`=8, `K`=[64,128,256]) network
+Furthermore, compared to experiment 1.3, where the 12-layer (`L=4`, `K=[64,128,256]`) network was unable to learn,
+the same architecture using ResNet again performed very well, and even the 24-layer (`L=8`, `K=[64,128,256]`) network
 was able to converge. This emphasizes how capable the skip connections are in preventing vanishing gradients.
 
 """
 
 part3_q5 = r"""
 **Your answer:**
+1. Our main modifications to the architecture were the following:
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+    ##### <u>BatchNorm layer after each activation function:</u>
+    Since we want our architecture to support deeper networks, we concluded that we need some way to prevent the
+    vanishing gradient phenomenon. As we explained in Q1, one way to try to resolve this issue is by using batch 
+    normalization, which helps stabilizing the gradients.
+    
+    ##### <u>Dropout layer after every MaxPool layer:</u>
+    As we learned in class (and empirically witnessed in part 2), dropout layers can be used to help improve the 
+    generalization of networks by reducing overfitting. Thus, we hoped adding this feature to our architecture 
+    would help improve its accuracy.
+    
+    ##### <u>No hidden layers in the classifier:</u>
+    After empirically testing several different architectures with different configurations and comparing their results, 
+    we eventually decided not to use any hidden layers in the classifier. 
+    
+        
+2. We tested our architecture on all configurations using: `K=[32,64,128]` and `L=[2,3,6,9,12]`. Since most of these 
+configurations create pretty deep networks (for example using `L=12` leads to 36 convolutional layers), we weren't
+sure whether some of these networks would be able to learn at all given that significantly shorter networks weren't able
+to in the previous experiments. To our surprise, all the networks we tested using this architecture, no matter how deep,
+were able to learn this time, which proves how capable batch normalization is in preventing vanishing gradients. 
+Also, in its best configuration (`L=3`) our architecture peaked at 84.9% test accuracy, which is significantly better 
+than even the best ResNet configuration we tested in experiment 1.4, which barely hit 80%. We think this improvement in
+accuracy can be attributed to the fact that our architecture, unlike previous architectures, utilizes dropout layers 
+which improves its generalization.
 """
 # ==============
