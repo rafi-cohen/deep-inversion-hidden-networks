e683317016146ba6a65031e922387c039a8356d8
rafi-cohen <77580049+rafi-cohen@users.noreply.github.com>
2020-01-10 18:26:26 +0200

    Fix Decoder to use ConvTranspose2d

--- hw3/hw3/autoencoder.py
+++ hw3/hw3/autoencoder.py
@@ -63,22 +63,22 @@ class DecoderCNN(nn.Module):
         #  inputs to the Encoder were.
         # ====== YOUR CODE: ======
         modules = [
-            nn.Conv2d(in_channels=in_channels, out_channels=256, kernel_size=5, stride=1, padding=2, dilation=1),
+            nn.ConvTranspose2d(in_channels=in_channels, out_channels=256, kernel_size=5, stride=1, padding=2, dilation=1),
             nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
             nn.BatchNorm2d(num_features=256),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=5, stride=1, padding=2, dilation=1),
+            nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=5, stride=1, padding=2, dilation=1),
             nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
             nn.BatchNorm2d(num_features=128),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=5, stride=1, padding=2, dilation=1),
+            nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=5, stride=1, padding=2, dilation=1),
             nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
             nn.BatchNorm2d(num_features=64),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=64, out_channels=out_channels, kernel_size=5, stride=1, padding=2, dilation=1),
+            nn.ConvTranspose2d(in_channels=64, out_channels=out_channels, kernel_size=5, stride=1, padding=2, dilation=1),
             nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
             nn.BatchNorm2d(num_features=out_channels),
             nn.ReLU()
