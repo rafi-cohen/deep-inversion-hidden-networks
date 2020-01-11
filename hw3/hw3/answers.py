5416795c46837ce63da8748b4ce3c232554295be
rafi-cohen <77580049+rafi-cohen@users.noreply.github.com>
2020-01-11 20:50:43 +0200

    VAE Final

--- hw3/hw3/answers.py
+++ hw3/hw3/answers.py
@@ -98,7 +98,7 @@ PART2_CUSTOM_DATA_URL = None
 def part2_vae_hyperparams():
     hypers = dict(
         batch_size=0,
-        h_dim=0, z_dim=0, x_sigma2=0,
+        h_dim=0, z_dim=0, x_sigma2=0.0005,
         learn_rate=0.0, betas=(0.0, 0.0),
     )
     # DONE: Tweak the hyperparameters to generate a former president.
