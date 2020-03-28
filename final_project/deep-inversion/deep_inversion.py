9eb54999c936f2ef8a73ef6a6d83ddc6aeb7212f
rafi-cohen <77580049+rafi-cohen@users.noreply.github.com>
2020-03-28 19:45:54 +0300

    Add progress bar Remove irrelevant preprocessing Fix image saving Fix layer output capture by hooks Fix batch transformations Fix passing output to regularization instead of input

--- final_project/deep-inversion/deep_inversion.py
+++ final_project/deep-inversion/deep_inversion.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from torchvision import models, transforms
 import numpy as np
 from PIL import Image
+from tqdm import tqdm
 from utils import DIRegularization
 
 
@@ -29,11 +30,7 @@ class DeepInvert:
             std=self.transformStd
         )
 
-        self.transformPreprocess = transforms.Compose([
-            transforms.Resize((imgSize, imgSize)),
-            transforms.ToTensor(),
-            self.transformNormalise
-        ])
+        self.transformPreprocess = self.transformNormalise
 
         self.tensorMean = torch.Tensor(self.transformMean)
         if cuda:
@@ -44,40 +41,68 @@ class DeepInvert:
             self.tensorStd = self.tensorStd.cuda()
 
     def toImages(self, input):
-        input.transpose_(1, 2)
-        input.transpose_(2, 3)
         images = []
-        for image in input:
-            normalized = image * self.tensorStd + self.tensorMean
-            clipped = np.clip(normalized, 0, 1)
-            images.append(Image.fromarray(np.uint8(clipped * 255)))
+        with torch.no_grad():
+            input.transpose_(1, 2)
+            input.transpose_(2, 3)
+            for image in input:
+                normalized = (image * self.tensorStd + self.tensorMean).cpu()
+                clipped = np.clip(normalized, 0, 1)
+                images.append(Image.fromarray(np.uint8(clipped * 255)))
         return images
 
-    def deepInvert(self, batch, iterations, target_onehot, lr):
-        transformed = self.transformPreprocess(batch)
+    def forward(self, input):
+        batch_running_means = []
+        batch_running_vars = []
+
+        def hook(module, input, output):
+            if isinstance(module, nn.Conv2d):
+                shape = output.shape
+                batch_running_means.append(output.view(shape[0], shape[1], -1).mean(dim=2).mean(dim=0))
+                if shape[0] == 1:
+                    batch_running_vars.append(0)
+                else:
+                    batch_running_vars.append(output.view(shape[0], shape[1], -1).var(dim=2).var(dim=0))
+
+        handles = []
+        for module in self.model.modules():
+            handle = module.register_forward_hook(hook)
+            handles.append(handle)
+
+        output = self.model(input)
+
+        for handle in handles:
+            handle.remove()
+
+        return output, batch_running_means, batch_running_vars
+
+    def deepInvert(self, batch, iterations, target_criterion, lr):
+        transformed_images = []
+        for image in batch:
+            transformed_images.append(self.transformPreprocess(image))
+        transformed = torch.stack(transformed_images)
         if self.cuda:
             transformed = transformed.cuda()
         input = torch.autograd.Variable(transformed, requires_grad=True)
         # initialize the optimizer and register the image as a parameter
-        optimizer = optim.Adam(input, lr)
-        for _ in range(iterations):
-            output = input
-            batch_running_means = []
-            batch_running_vars = []
-            for module in self.model.modules:
-                output = module(output)
-                if isinstance(module, nn.Conv2d):
-                    batch_running_means.append(torch.mean(output, dim=0))
-                    batch_running_vars.append(torch.var(output, dim=0))
-            optimizer.zero_grad()
-            loss = self.loss_fn(output, target_onehot)
-            loss += self.reg_fn(output, batch_running_means,
-                                batch_running_vars)
-            # R_ADI = R_DI + R_compete
-            loss.backward()
-            optimizer.step()
+        optimizer = optim.Adam([input], lr)
+        output = []
+        with tqdm(total=iterations) as pbar:
+            for i in range(iterations):
+                output, batch_running_means, batch_running_vars = self.forward(input)
+                optimizer.zero_grad()
+                loss = self.loss_fn(output, target_criterion)
+                loss += self.reg_fn(input, batch_running_means,
+                                    batch_running_vars)
+                # R_ADI = R_DI + R_compete
+                loss.backward()
+                optimizer.step()
 
-        return self.toImages(output)
+                desc_str = f'#{i}: loss = {loss}'
+                pbar.set_description(desc_str)
+                pbar.update()
+
+        return self.toImages(input)
 
 # class AdaptiveDeepInvert(DeepInvert):
 #     def __init__(self, image, cuda, a_tv, a_l2):
