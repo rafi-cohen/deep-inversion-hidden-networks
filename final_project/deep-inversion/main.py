4630847f3140985fe7e7a846b6e2afcc9e30ff29
rafi-cohen <77580049+rafi-cohen@users.noreply.github.com>
2020-03-28 19:47:01 +0300

    Fix main file: Add call to main() Move tensors to cuda Fix target criterion

--- final_project/deep-inversion/main.py
+++ final_project/deep-inversion/main.py
@@ -4,8 +4,8 @@ from PIL import Image
 from deep_inversion import DeepInvert
 
 CUDA_ENABLED = torch.cuda.is_available()
-HYPERPARAMS = dict(batch_size=2,
-                   target_class=957,
+HYPERPARAMS = dict(batch_size=1,
+                   target_class=933,
                    lr=0.05,
                    a_tv=1e-4,
                    a_l2=0,
@@ -17,11 +17,17 @@ FILENAME_FORMAT = path.join(OUT_DIR, '{}.jpg')
 
 def main():
     batch = torch.randn(HYPERPARAMS['batch_size'], 3, 224, 224)
-    target_onehot = torch.zeros(1000)
-    target_onehot[HYPERPARAMS['target_class']] = 1
+    target_criterion = HYPERPARAMS['target_class'] * torch.ones(HYPERPARAMS['batch_size'], dtype=torch.long)
+    if CUDA_ENABLED:
+        batch = batch.cuda()
+        target_criterion = target_criterion.cuda()
     DI = DeepInvert(batch, CUDA_ENABLED,
                     HYPERPARAMS['a_tv'], HYPERPARAMS['a_l2'], HYPERPARAMS['a_f'])
-    images = DI.deepInvert(batch, iterations=2000,
-                           target_onehot=target_onehot, lr=HYPERPARAMS['lr'])
+    images = DI.deepInvert(batch, iterations=800,
+                           target_criterion=target_criterion, lr=HYPERPARAMS['lr'])
     for i, image in enumerate(images):
         image.save(FILENAME_FORMAT.format(i))
+
+
+if __name__ == '__main__':
+    main()
